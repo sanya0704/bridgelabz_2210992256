170c673424aca2f38545691079058651ad780d2b
sanya <sanyabansal.ynr@gmail.com>
2025-06-30 12:57:29 +0530

    add text file

---
+++ name.java
@@ -0,0 +1,14 @@
+public class Myclass {
+    public static void main(String[] args) {
+        System.out.println("shreya");
+
+        int a = 1;
+        int b = 2;
+        int c = a + b;
+        int d = a - b;
+
+        System.out.println("Addition (a + b): " + c); // Outputs: 3
+        System.out.println("Subtraction (a - b): " + d); // Outputs: -1
+        System.out.println("2210990841");
+    }
+}
